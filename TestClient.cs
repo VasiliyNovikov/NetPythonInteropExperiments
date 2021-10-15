c1397c3c7cb6b26890aafb56a6164c0d6a3fc3cc
Vasiliy Novikov <vanoviko@microsoft.com>
2021-10-15 12:31:10 +0000

    Try dictionary args

--- TestClient.cs
+++ TestClient.cs
@@ -6,6 +6,19 @@ namespace NetPythonInteropExperiments
     {
         private readonly string _name;
         public TestClient(string name) => _name = name;
+
         public void Add(int a, int b) => Console.WriteLine($"{_name}: {a} + {b} = {a + b}");
+
+        public void Call(string method, dynamic args, dynamic kwargs)
+        {
+            Console.WriteLine($"{_name} - Invoke method {method}:");
+            Console.WriteLine("  args:");
+            foreach (var arg in args)
+                Console.WriteLine($"    {arg}");
+            Console.WriteLine("  kwargs:");
+            Console.WriteLine(kwargs);
+            foreach (var kwarg in kwargs.items())
+                Console.WriteLine($"    {kwarg[0]}: {kwarg[1]}");
+        }
     }
 }
--- test.py
+++ test.py
@@ -10,6 +10,7 @@ def test(context, client1, client2):
     print("Hello 2 clients: %s" % context['session_id'])
     client1.Add(3, 4)
     client2.Add(3, 4)
+    client1.Call('test_method', [42, 'test arg'], dict(key1='value 1', key42=42))
 
 
 def teardown(context, client1, client2):
